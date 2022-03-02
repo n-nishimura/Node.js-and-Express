970f527384f564f8b94c8d21de0cfc744a61875a
n-nishimura <n.nishimurara@gmail.com>
2022-03-02 23:56:51 +0900

    coronaData

--- index.js
+++ index.js
@@ -3,13 +3,15 @@
 
 const express = require("express");
 const app = express();
+const coronaData = require("./coronaData.json");
 
 
-app.get("/",function (req,res){
-  res.send("こんにちは");
-  });
+app.get("/",function(req, res){
+  res.send(coronaData);
+});
 
 
+//port 5000はAirPlayと競合
 app.listen(5000, function(){
   console.log("Listening on localhost port 5000");
 });
