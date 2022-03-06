e7715d8d44564fe59488d8037728dc8eac813e63
n-nishimura <n.nishimurara@gmail.com>
2022-03-06 22:59:56 +0900

    POSTリクエストの確認

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/",function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/autumn", function(req,res){
+  console.log("postの確認")
+});
+
 
 //port 5000はAirPlayと競合
 app.listen(5000, function(){
