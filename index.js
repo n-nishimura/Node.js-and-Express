50c2b6fd3c35ad098c74e25a1b3ecd00002d1bdd
n-nishimura <n.nishimurara@gmail.com>
2022-03-11 23:58:32 +0900

    データベースへの書き込み

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
 //https://qiita.com/masato44gm/items/dffb8281536ad321fb08
 //restAPI
-
+const fs = require("fs")
 const express = require("express");
 const app = express();
-
+app.use(express.urlencoded({extended : true }));
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/autumn", function(req,res){
-  console.log("postの確認")
+  fs.writeFile(__dirname + "/date.txt", res.body.activity , function(){
+    res.send("投稿完了");
+  });
 });
 
 
