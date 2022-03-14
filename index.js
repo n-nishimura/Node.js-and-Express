01c79595ead342fdd7a8fc0e9528ebe2b1298e92
n-nishimura <n.nishimurara@gmail.com>
2022-03-14 23:18:30 +0900

    update

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const fs = require("fs")
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended : true }));
+const activities = require("./activities.json")
 
 app.get("/",function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/autumn", function(req,res){
-  fs.writeFile(__dirname + "/date.txt", res.body.activity , function(){
+  fs.writeFile(__dirname + "/data.txt", req.body.activity , function(){
     res.send("投稿完了");
   });
 });
 
+app.get("/update",function(req,res){
+  res.send(activities);
+});
 
 //port 5000はAirPlayと競合
 app.listen(5000, function(){
